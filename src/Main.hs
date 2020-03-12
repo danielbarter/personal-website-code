d83d6dbda4da0a58979ccdf4eab09c60cfd4cc88
Daniel Barter <danielbarter@gmail.com>
2020-03-11 18:39:04 -0700

    defined createDirectoryTree

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,18 @@
 module Main where
 
-import System.Directory (createDirectoryIfMissing)
+import System.Directory ( createDirectoryIfMissing
+                        , setCurrentDirectory
+                        )
 
 main :: IO ()
-main = createDirectoryIfMissing False "site"
+main = createDirectoryTree
+
+
+createDirectoryTree :: IO ()
+createDirectoryTree = do createDirectoryIfMissing False "site"
+                         setCurrentDirectory "./site"
+                         createDirectoryIfMissing False "gif"
+                         createDirectoryIfMissing False "img"
+                         createDirectoryIfMissing False "js"
+                         createDirectoryIfMissing False "pdf"
+                         setCurrentDirectory "../"
