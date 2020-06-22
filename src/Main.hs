b249a9878e47aab408dec595dac8ef0ec8b7e804
Daniel Barter <danielbarter@gmail.com>
2020-06-22 13:56:37 -0700

    announcing fusion

--- src/Main.hs
+++ src/Main.hs
@@ -33,6 +33,7 @@ main =
   do setCurrentDirectory "/home/danielbarter/personal-website-code"
      createDirectoryTree
      processText ( Right . id ) "./content/html/home.html" "./site/index.html"
+     processText ( Right . id ) "./content/html/fusion.html" "./site/fusion.html"
      pmd "./content/markdown/CV.md" "./site/CV.html"
      cvTex "./content/markdown/CV.md" "./content/tex/CV.tex"
      pmd "./content/markdown/publickey.md" "./site/publickey.html"
