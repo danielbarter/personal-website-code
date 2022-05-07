3b44416a0358b0b9638a72b8339bee22a8d9f5d3
Daniel Barter <danielbarter@gmail.com>
2022-05-07 16:26:53 -0700

    saddles post

--- src/Main.hs
+++ src/Main.hs
@@ -38,6 +38,7 @@ main =
      cvTex "./content/markdown/CV.md" "./content/tex/CV.tex"
      pmd "./content/markdown/publickey.md" "./site/publickey.html"
      pmd "./content/markdown/mix.md" "./site/mix.html"
+     pmd "./content/markdown/saddles.md" "./site/saddles.html"
      pmd "./content/markdown/tableau.md" "./site/tableau.html"
      copy "./content/js/mix.js" "./site/js/mix.js"
      copy "./content/js/tableau.js" "./site/js/tableau.js"
