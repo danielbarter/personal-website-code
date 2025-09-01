e82644f2b400ea87a7479788ad2e73b7f788e4a9
Daniel Barter <danielbarter@gmail.com>
2025-09-01 10:01:12 -0700

    bumping env

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-with (import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/heads/nixos-24.11.tar.gz") {});
+with (import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/heads/nixos-25.05.tar.gz") {});
 let hpkgs = p: [ p.base
                  p.directory
                  p.filepath
